0e85b976543772f6a75b9d1e42c0c9e653b96708
yarhoo <yarhoo@vip.qq.com>
2017-03-21 14:51:23 +0800

    filelistentity fix 2

--- app/src/main/java/com/chd/base/Entity/FilelistEntity.java
+++ app/src/main/java/com/chd/base/Entity/FilelistEntity.java
@@ -18,7 +18,10 @@ public class FilelistEntity {
     private int unBakNumber = 0;
     private HashMap<String, Integer> backmap;
     private HashMap<Integer,String> pathmap;
-    private final List<Integer> _GroupCp ;
+    private final List<Integer> _GroupCpLocal;
+    private final List<Integer> _GroupCpCloud;
+    private final String classname="FilelistEntity";
+    private int _gpinterval;
 
 
 
@@ -31,7 +34,8 @@ public class FilelistEntity {
         pathmap=new HashMap<>();
        // bklist=new ArrayList<>();
         locallist=new ArrayList<>();
-        _GroupCp = new ArrayList<>();
+        _GroupCpLocal = new ArrayList<>();
+        _GroupCpCloud=new ArrayList<>();
     }
 
     public void ReSet()
@@ -39,7 +43,9 @@ public class FilelistEntity {
         backmap.clear();
         pathmap.clear();
         locallist.clear();
-        _GroupCp.clear();
+        _GroupCpLocal.clear();
+        _GroupCpLocal.clear();
+        _gpinterval=0;
     }
 
 
@@ -156,21 +162,34 @@ public class FilelistEntity {
         final  int interval=intervalDay*24*3600;
         int lastday=0,day=0;
         int idx=0,pst=0;
-        if (_GroupCp.size()-1>(postion)) {
+        if (list.size()<1) {
+            Log.i(classname,"list is empty");
+            return null;
+        }
+        List<Integer> GroupLL = null;
+        if (list.get(0) instanceof  FileLocal)
+            GroupLL=_GroupCpLocal;
+        else
+            GroupLL=_GroupCpCloud;
+        if (_gpinterval!=interval) {
+            GroupLL.clear();
+            _gpinterval=interval;
+        }
+        if (GroupLL.size()-1>(postion)) {
             //pst=postion+1;
-            pst=_GroupCp.get(postion)+1;
+            pst= GroupLL.get(postion)+1;
             idx=list.size();
-            Log.i("FilelistEntity","use cache ing ...");
+            Log.i(classname,"use cache ing ...");
         }
         else {
-            if (_GroupCp.size()>0)
+            if (GroupLL.size()>0)
             {
-                pst = _GroupCp.size()-1;
-                idx = _GroupCp.get(pst );
-                Log.i("FilelistEntity","use cache last idx ");
+                pst = GroupLL.size()-1;
+                idx = GroupLL.get(pst );
+                Log.i(classname,"use cache last idx ");
                 if (idx+1>=list.size())
                 {
-                    Log.i("FilelistEntity", "reach buttom  ");
+                    Log.i(classname, "reach buttom  ");
                     return null;
                 }
                 lastday=list.get(idx++).getLastModified();
@@ -179,14 +198,14 @@ public class FilelistEntity {
             else
                 lastday=list.get(0).getLastModified();
         }
-        Log.i("FilelistEntity "," postion from cache :"+ pst);
+        Log.i(classname," postion from cache :"+ pst);
         for(;( (idx<list.size()) && pst<=postion );idx++)
         {
             day=list.get(idx).getLastModified();
                 if (day-lastday>interval)
                 {
-                    Log.i("FilelistEntity found","found idx:"+idx);
-                    _GroupCp.add(pst,idx);
+                    Log.i(classname ,"found idx:"+idx);
+                    GroupLL.add(pst,idx);
                     pst++;
 
                 }
@@ -194,10 +213,10 @@ public class FilelistEntity {
         }
         if (pst-1!=postion)
         {
-            Log.i("FilelistEntity ","not found postion: "+postion+" pst:"+pst);
-            if (_GroupCp.size()>1)
+            Log.i(classname,"not found postion: "+postion+" pst:"+pst);
+            if (GroupLL.size()>1)
                 return null;
-            _GroupCp.add(pst,idx-1);
+            GroupLL.add(pst,idx-1);
             pst=pst+1;
 
         }
@@ -207,16 +226,18 @@ public class FilelistEntity {
         if (postion==0)
             ret[0]=0;
         else
-            ret[0]=Math.max(1,_GroupCp.get(frontPst)+1);
-        ret[1]=Math.max(0,_GroupCp.get(currentPst));
+            ret[0]=Math.max(1, GroupLL.get(frontPst)+1);
+        ret[1]=Math.max(0, GroupLL.get(currentPst))+1;
 
-       /* Log.i("FilelistEntity ","Current :"+ currentPst+" "+ ret[0]+" "+ret[1]);
-        Log.i("FilelistEntity",""+ TimeUtils.getDayWithTimeMillis0(list.get(ret[0]).getLastModified())+" === "+TimeUtils.getDayWithTimeMillis0(list.get(ret[1]).getLastModified()));
+       Log.i(classname,"Current :"+ currentPst+" "+ ret[0]+" "+ret[1]);
+        //Log.i(classname,""+ TimeUtils.getDayWithTimeMillis0(list.get(ret[0]).getLastModified())+" === "+TimeUtils.getDayWithTimeMillis0(list.get(ret[1]).getLastModified()));
+/*
 
-        for (int i=0;i<_GroupCp.size();i++)
+        for (int i = 0; i< GroupLL.size(); i++)
         {
-            Log.i("FilelistEntity"," idx: "+ i+" : "+_GroupCp.get(i));
-        }*/
+            Log.i(classname," idx: "+ i+" : "+ GroupLL.get(i));
+        }
+*/
 
 
         return ret;
