3f31c46d6dd435cb8bd8bb7e88d485269f9f1082
yarhoo <yarhoo@vip.qq.com>
2017-03-21 15:13:37 +0800

    add attribkey

---
+++ app/src/main/java/com/chd/base/Entity/AttribKey.java
@@ -0,0 +1,13 @@
+package com.chd.base.Entity;
+
+/**
+ * Created by lxp on 2017/3/21.
+ */
+
+public  enum  AttribKey {
+
+    duration {public String getName(){return "duration";}}
+    ,title  {public  String getName() {return  "title";}}
+    ,lines  {public  String getName() {return  "lines";}}
+
+}
