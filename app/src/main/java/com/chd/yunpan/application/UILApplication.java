f5b4a06674d00ef9667d79254362a092d9e97165
DESKTOP-HURHI3T\liuxp <lxp@assetzz.com>
2018-01-13 11:11:31 +0800

    upload thread pool fix

--- app/src/main/java/com/chd/yunpan/application/UILApplication.java
+++ app/src/main/java/com/chd/yunpan/application/UILApplication.java
@@ -163,14 +163,14 @@ public class UILApplication extends Application {
 
     private static void initDownUP(Context context) {
 
-        ExecutorService exService = Executors.newCachedThreadPool();
+        //ExecutorService exService = Executors.newFixedThreadPool(2);
         //ExecutorService exService = Executors.newFixedThreadPool(2);
 
         FileUploadConfiguration fileUploadConfiguration = new FileUploadConfiguration.Builder(context)
                 .setResponseProcessor(new TrpcResponseParse())  //设置http response字符串的结果解析器，如果不设置，则默认返回response字符串
                 .setThreadPoolSize(2)         //设置线程池大小，如果采用默认的线程池则有效
                 .setThreadPriority(Thread.NORM_PRIORITY - 1)  //设置线程优先级，如果采用默认的线程池则有效
-                .setTaskExecutor(exService)     //设置自定义的线程池
+               // .setTaskExecutor(exService)     //设置自定义的线程池
                 .setFileUploader(new TrpcUploader())    //设置自定义的文件上传功能，如果不设置则采用默认的文件上传功能
                 .build();
         FileUploadManager.getInstance().init(fileUploadConfiguration);
