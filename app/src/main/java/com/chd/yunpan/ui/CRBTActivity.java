617e0559171b4e51422be0ab8d6bc0a1096246d2
liumengjie <jerome_android@163.com>
2018-01-29 11:38:39 +0800

    提交修改

--- app/src/main/java/com/chd/yunpan/ui/CRBTActivity.java
+++ app/src/main/java/com/chd/yunpan/ui/CRBTActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,7 +17,7 @@ import com.chd.yunpan.R;
 public class CRBTActivity extends Activity implements OnClickListener {
 	private ImageView mIvLeft;
 	private TextView mTvCenter;
-//	private WebView crbt_webview;
+	private WebView crbt_webview;
 	private Activity activity;
 
 	@Override
@@ -26,37 +30,58 @@ public class CRBTActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_crbt);
 		initTitle();
 		initListener();
-//		String url = "http://box.10155.com/";
-//		crbt_webview.loadUrl(url);
-//		activity=this;
-//
-//		crbt_webview.setWebChromeClient(new WebChromeClient(){
-//			@Override
-//			public void onProgressChanged(WebView view, int newProgress) {
-//				activity.setTitle("Loading...");
-//				activity.setProgress(newProgress * 100);
-//				if(newProgress == 100)
-//					setProgressBarIndeterminateVisibility(false); // Hide progress circle when page loaded
-//				activity.setTitle("My title");
-//			}
-//		});
-//
-//		WebSettings webSettings = crbt_webview.getSettings();
-//		webSettings.setUseWideViewPort(true);//设置此属性，可任意比例缩放
-//		webSettings.setLoadWithOverviewMode(true);
-//		webSettings.setLoadsImagesAutomatically(true);
-//		webSettings.setJavaScriptEnabled(true);
-//		webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+		String url = "http://more.i8855.cn:8080/";
+		crbt_webview.loadUrl(url);
+		activity=this;
+		crbt_webview.setWebViewClient(new WebViewClient() {
+			public boolean shouldOverrideUrlLoading(WebView view, String url)
+			{ //  重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边
+				view.loadUrl(url);
+				return true;
+			}
+		});
+
+		crbt_webview.setWebChromeClient(new WebChromeClient(){
+
+			@Override
+			public void onProgressChanged(WebView view, int newProgress) {
+				activity.setTitle("Loading...");
+				activity.setProgress(newProgress * 100);
+				if(newProgress == 100)
+					setProgressBarIndeterminateVisibility(false); // Hide progress circle when page loaded
+				activity.setTitle("更多精彩");
+			}
+		});
+
+		WebSettings webSettings = crbt_webview.getSettings();
+		webSettings.setLoadWithOverviewMode(true);
+		webSettings.setLoadsImagesAutomatically(true);
+		webSettings.setJavaScriptEnabled(true);
+		//自适应屏幕
+		webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+		webSettings.setLoadWithOverviewMode(true);
+		webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+		webSettings.setDomStorageEnabled(true);
+
+		webSettings.setAppCacheMaxSize(1024*1024*8);
+
+		String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+
+		webSettings.setAppCachePath(appCachePath);
+
+		webSettings.setAllowFileAccess(true);
+
+		webSettings.setAppCacheEnabled(true);
 
 
 	}
 
 
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-//		if ((keyCode == KeyEvent.KEYCODE_BACK) && crbt_webview.canGoBack()) {
-//			crbt_webview.goBack();
-//			return true;
-//		}
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && crbt_webview.canGoBack()) {
+			crbt_webview.goBack();
+			return true;
+		}
 		return super.onKeyDown(keyCode, event);
 	}
 
@@ -67,7 +92,7 @@ public class CRBTActivity extends Activity implements OnClickListener {
 	private void initTitle() {
 		mIvLeft = (ImageView) findViewById(R.id.iv_left);
 		mTvCenter = (TextView) findViewById(R.id.tv_center);
-//		crbt_webview = (WebView) findViewById(R.id.crbt_webview);
+		crbt_webview = (WebView) findViewById(R.id.crbt_webview);
 
 		mTvCenter.setText("更多精彩");
 	}
